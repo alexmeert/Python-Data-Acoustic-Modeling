82e84d74f2c8f9e00eefd8742f9fd25acb4d4f30
alexmeert <115733315+alexmeert@users.noreply.github.com>
2023-12-04 10:09:51 -0500

    Update view.py

--- view.py
+++ view.py
@@ -1,1 +1,16 @@
+# view.py will deal with all things visual like the tkinter GUI
+import tkinter as tk
+from tkinter import filedialog
 
+class AudioConverterView:
+    def __init__(self, controller):
+        self.controller = controller
+        self.window = tk.Tk()
+        self.window.title("Clap Audio Analysis")
+        self.window.geometry("300x100")
+
+        self.button = tk.Button(self.window, text="Open File Dialog", height=2, width=20, command=self.controller.open_file_dialog)
+        self.button.pack(side="top", pady=20)
+
+    def run(self):
+        self.window.mainloop()
