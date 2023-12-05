75301c8c634c86ab8bde9bb0c695871bef7bfa12
TheRealKenergy <150475584+TheRealKenergy@users.noreply.github.com>
2023-12-05 12:04:28 -0500

    Update view.py
    made the gui bigger

--- view.py
+++ view.py
@@ -7,7 +7,7 @@ class AudioConverterView:
         self.controller = controller
         self.window = tk.Tk()
         self.window.title("Clap Audio Analysis")
-        self.window.geometry("300x100")
+        self.window.geometry("300x200")
 
         self.button = tk.Button(self.window, text="Open File Dialog", height=2, width=20, command=self.controller.open_file_dialog)
         self.button.pack(side="top", pady=20)
