7527b48e6d9805f80ada0ce4549e5e7ee838a93f
TheRealKenergy <150475584+TheRealKenergy@users.noreply.github.com>
2023-12-05 12:07:28 -0500

    Update view.py
    made it look better

--- view.py
+++ view.py
@@ -7,12 +7,12 @@ class AudioConverterView:
         self.controller = controller
         self.window = tk.Tk()
         self.window.title("Clap Audio Analysis")
-        self.window.geometry("300x200")
+        self.window.geometry("300x150")
 
         self.button = tk.Button(self.window, text="Open File Dialog", height=2, width=20, command=self.controller.open_file_dialog)
         self.button.pack(side="top", pady=20)
 
-        self.close_button = tk.Button(self.window, text="Close", command=self.window.destroy)
+        self.close_button = tk.Button(self.window, text="Exit", command=self.window.destroy, fg = "red")
         self.close_button.pack(side="bottom", pady=10)
 
     def run(self):
