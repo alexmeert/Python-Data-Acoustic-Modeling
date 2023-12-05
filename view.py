c2b930e775f8d206d73a397efba3065f00cf9196
TheRealKenergy <150475584+TheRealKenergy@users.noreply.github.com>
2023-12-05 12:01:37 -0500

    Update view.py
    added the close button to the gun

--- view.py
+++ view.py
@@ -12,5 +12,20 @@ class AudioConverterView:
         self.button = tk.Button(self.window, text="Open File Dialog", height=2, width=20, command=self.controller.open_file_dialog)
         self.button.pack(side="top", pady=20)
 
+        self.close_button = tk.Button(self.window, text="Close", command=self.window.destroy)
+        self.close_button.pack(side="bottom", pady=10)
+
     def run(self):
         self.window.mainloop()
+
+class AudioConverterController:
+    def __init__(self):
+        self.view = AudioConverterView(self)
+
+    def open_file_dialog(self):
+        file_path = filedialog.askopenfilename()
+        print("Selected file:", file_path)
+
+if __name__ == "__main__":
+    controller = AudioConverterController()
+    controller.view.run()
