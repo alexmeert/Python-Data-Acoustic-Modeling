c0aa9d15086fe68f7fc3c8053aeaadcbf597d769
alexmeert <115733315+alexmeert@users.noreply.github.com>
2023-12-09 21:59:53 -0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,13 +7,15 @@ class AudioConverterController:
         self.model = AudioConverter()
         self.view = AudioConverterView(self)
 
+    def convert_audio(self, src, dst):
+        return self.model.convert_audio(src, dst)
+
     def open_file_dialog(self):
-        file_path = filedialog.askopenfilename(title="Select an audio file", filetypes=[("Audio files", "*.mp3")])
-        if file_path:
-            print(f"Selected file: {file_path}")
-            converted_file_path, duration_seconds = self.model.convert_audio(file_path, "pt_mono.wav")
-            self.view.update_duration_label(duration_seconds)
+        self.view.open_file_dialog()
+
+    def run(self):
+        self.view.run()
 
 if __name__ == "__main__":
     controller = AudioConverterController()
-    controller.view.run()
+    controller.run()
