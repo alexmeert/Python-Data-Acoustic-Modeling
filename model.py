d21322a0f0023e22e9fa22a40eaf3d4f192b0865
alexmeert <115733315+alexmeert@users.noreply.github.com>
2023-12-09 22:02:25 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,25 +1,19 @@
+# model.py
+
 from pydub import AudioSegment
 import os
-
 
 class AudioConverter:
     @staticmethod
     def convert_audio(src, dst):
         sound = AudioSegment.from_mp3(src)
         sound.export(dst, format="wav")
-
         raw_audio = AudioSegment.from_file(dst, format="wav")
         channel_count = raw_audio.channels
         print(f"Channel count before conversion: {channel_count}")
-
         mono_wav = raw_audio.set_channels(1)
         mono_wav.export("pt_mono.wav", format="wav")
-
         mono_wav_audio = AudioSegment.from_file("pt_mono.wav", format="wav")
         channel_count = mono_wav_audio.channels
         print(f"Channel count after conversion: {channel_count}")
-
-        # Get the duration of the mono audio in seconds
-        duration_seconds = len(mono_wav_audio) / 1000.0
-
-        return os.path.abspath("pt_mono.wav"), duration_seconds
+        return os.path.abspath("pt_mono.wav")
